4c2140fd2118bd980f13af92e2044c4f7b310af7
koooohei0906 <suikapan0906@gmail.com>
2025-12-10 12:29:43 +0900

    feat: 料理記録の新規作成機能（new/create）を追加

---
+++ app/controllers/cooking_records_controller.rb
@@ -0,0 +1,23 @@
+class CookingRecordsController < ApplicationController
+  before_action :authenticate_user!
+
+  def new
+    @cooking_record = CookingRecord.new
+  end
+
+  def create
+    @cooking_record = current_user.cooking_record.new(cooking_record_params)
+
+    if @cooking_record.save
+      redirect_to, root_path notice: "料理記録を登録しました"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def cooking_record_params
+    params.require(:cooking_record).permit(:cooked_on, :dish_name, :recipe_url, :memo)
+  end
+end
