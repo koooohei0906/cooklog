ea5b3e26a306554f1e34b0a53b610906ccc10404
koooohei0906 <suikapan0906@gmail.com>
2025-12-10 13:05:03 +0900

    chore: createメソッドの関連名を修正

--- app/controllers/cooking_records_controller.rb
+++ app/controllers/cooking_records_controller.rb
@@ -6,7 +6,7 @@ class CookingRecordsController < ApplicationController
   end
 
   def create
-    @cooking_record = current_user.cooking_record.new(cooking_record_params)
+    @cooking_record = current_user.cooking_records.new(cooking_record_params)
 
     if @cooking_record.save
       redirect_to root_path, notice: "料理記録を登録しました"
