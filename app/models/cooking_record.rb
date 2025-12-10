0b864ba6c5df962fb560e6eafe024c409a2f9687
koooohei0906 <suikapan0906@gmail.com>
2025-12-10 20:06:17 +0900

    chore: Lintエラー修正

--- app/models/cooking_record.rb
+++ app/models/cooking_record.rb
@@ -1,8 +1,8 @@
 class CookingRecord < ApplicationRecord
   belongs_to :user
 
-  validates :cooked_on, presence: { message: '日付を入力してください' }
-  validates :dish_name, presence: { message: '料理名を入力してください' }
-  validates :recipe_url, allow_blank: true, format: { with: URI::DEFAULT_PARSER.make_regexp(%w[http https]), message: '有効なURLを入力してください' }
-  validates :memo, length: { maximum: 255, message: '255文字以内で入力してください' }
+  validates :cooked_on, presence: { message: "日付を入力してください" }
+  validates :dish_name, presence: { message: "料理名を入力してください" }
+  validates :recipe_url, allow_blank: true, format: { with: URI::DEFAULT_PARSER.make_regexp(%w[http https]), message: "有効なURLを入力してください" }
+  validates :memo, length: { maximum: 255, message: "255文字以内で入力してください" }
 end
