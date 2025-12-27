9ceeff66492069cbee66f65febf24b3d93111e17
koooohei0906 <suikapan0906@gmail.com>
2025-12-28 00:18:11 +0900

    chore: ActiveStorageのvariant_processorを:vipsに明示した

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Cooklog
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.2
+    config.active_storage.variant_processor = :vips # vipsを使うという明示！
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
