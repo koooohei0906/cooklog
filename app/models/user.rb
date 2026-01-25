042485e90179598fa413ba9fece6d429a65321c8
koooohei0906 <suikapan0906@gmail.com>
2026-01-25 17:39:42 +0900

    chore: Userモデルを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,21 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: %i[google_oauth2]
 
   has_many :cooking_records, dependent: :destroy
 
   validates :name, presence: true
+  validates :uid, presence: true, uniqueness: { scope: :provider }, if: -> { uid.present? } # 通常ユーザーはスルーしGoogkeログインユーザーだけバリデーションを動かすためのif
+
+
+  # providerとuidを使ってユーザーを検索し、存在しなければ新規作成する
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.name = auth.info.name
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+    end
+  end
 end
